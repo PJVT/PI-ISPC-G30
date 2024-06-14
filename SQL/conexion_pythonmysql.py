bdcb94cb243191f7204ef25c00fa627ad2252384
Lourdes00 <118091168+Lourdes00@users.noreply.github.com>
2024-06-14 07:33:49 -0300

    Update conexion_pythonmysql.py

--- SQL/conexion_pythonmysql.py
+++ SQL/conexion_pythonmysql.py
@@ -3,14 +3,14 @@ import mysql.connector
 HOST = "localhost"
 USER = "root"#
 PASSWORD = "password"#
-BD = "instituto"#instituto
+BD = "instituto"
 
 mydb = mysql.connector.connect(
   host="localhost",user=USER,
   password=PASSWORD,database=BD
 )
 mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM Persona WHERE id=3")
+mycursor.execute("SELECT * FROM Persona WHERE id=%s")
 myresult = mycursor.fetchall()
 
 for x in myresult:
